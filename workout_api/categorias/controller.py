ba24f865fe23d55544a7cafdfe5da3a68b6d8ec3
Your Name <you@example.com>
2024-06-08 21:35:46 -0300

    controller

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -1,16 +1,21 @@
+from uuid import uuid4
 from fastapi import APIRouter, Body, status
+from pydantic import UUID4
+from sqlalchemy import select
+from workout_api.categorias.models import CategoriaModel
 from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
 from workout_api.contrib.dependencies import DatabaseDepency
+
 router = APIRouter()
 
 @router.post('/', 
-             summary='Criar novo Atleta'
+             summary='Criar uma nova Categoria'
              status_code=status.HTTP_201_CREATED
              response_model=CategoriaOut,
              )
 
 async def post(
-    db_session: DatabaseDepency, 
+    db_session: DatabaseDependency, 
     categoria_in:CategoriaIn  = Body(...)
     ) -> CategoriaOut:
     
@@ -22,5 +27,33 @@ async def post(
     
     return categoria_out
 
-    breakpoint()
-   
+@router.get('/', 
+             summary='Consultar todas as Categorias'
+             status_code=status.HTTP_201_OK,
+             response_model=LIST[CategoriaOut],
+             )
+
+async def query(
+    db_session: DatabaseDepency, ) -> list[CategoriaOut]:
+    categorias:list[CategoriaOut] = (await db_session.execute(select(CategoriaModel))).scalars().all()
+    
+    return categorias
+
+@router.get('/{id}', 
+             summary='Consultar uma Categoria pelo id'
+             status_code=status.HTTP_201_OK,
+             response_model=LIST[CategoriaOut],
+             )
+
+async def query( id: UUID4, db_session: DatabaseDepency, ) -> CategoriaOut:
+    categorias:CategoriaOut = (await db_session.execute(select(CategoriaModel).filter_by(id=id))
+    ).scalars().first()
+    
+    if not categorias:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Categoria não encontrada no id: {id}'
+        )
+    
+    return categorias
+    
